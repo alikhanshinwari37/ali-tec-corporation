a56c85606a57cf2f9c22e2840e92783f48b772f5
alikhanshinwari37 <337tiger337@gmail.com>
2026-08-23 08:11:23 -0700

    Fix Gmail SMTP connection

--- server.js
+++ server.js
@@ -27,7 +27,9 @@ app.use(express.static(__dirname));
 
 // Gmail transporter
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
